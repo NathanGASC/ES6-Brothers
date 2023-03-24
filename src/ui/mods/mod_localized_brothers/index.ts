ce2c2a219dcdd4d1747bc6bc63ef68b944aacfaf
Nathan GASC <nathangasc17@gmail.com>
2023-03-24 04:21:28 +0100

    clean code but lose performance. Why?

--- src/ui/mods/mod_localized_brothers/index.ts
+++ src/ui/mods/mod_localized_brothers/index.ts
@@ -3,17 +3,16 @@ import 'core-js/actual/array/virtual/find-index';
 // @ts-ignore: File is not a module
 import WorkerTranslator from "worker-loader!./translator.worker";
 
-import fr from "./translations/fr.json"
-import en from "./translations/en.json"
-import { escapeRegExp } from "./utils/escapeRegExp"
+export type TranslateStringCallback = { translation: string, key: string }
+export type GetKeyFor = { key: string }
+export type GetValueFor = { value: string }
 
 class LocalizedBrothers {
   sqHandle: any = null
   static id = "localized_brothers";
-  dictionary = { fr, en }
-  private currentLang: keyof typeof this.dictionary = "en"
-  private workerTranslator:Worker = new WorkerTranslator()
-  private uniqueWorkerId:number = 0;
+  private currentLang: string = "en"
+  private workerTranslator: Worker = new WorkerTranslator()
+  private uniqueWorkerId: number = 0;
 
   constructor() {
 
@@ -45,7 +44,7 @@ class LocalizedBrothers {
     var orignalInit = (window as any).$.prototype.init;
     (window as any).$.prototype.init = (selector: any, context: any) => {
       var instanceJquery = new orignalInit(selector, context)
-      if(instanceJquery[0] instanceof HTMLElement) {
+      if (instanceJquery[0] instanceof HTMLElement) {
         this.translateElement(instanceJquery[0], "en", this.currentLang)
       }
       return instanceJquery
@@ -56,8 +55,50 @@ class LocalizedBrothers {
    * Short name for "setCurrentLang"
    * @param lang The new lang you want to use (ex:"fr", "en", "ja", ...)
    */
-  setLang(lang: keyof typeof this.dictionary) {
+  setLang(lang: string) {
     this.setCurrentLang(lang)
+  }
+
+  /**
+   * Get a translation using a key for the current lang
+   * @param key the key of the wanted translation
+   * @returns the tranlsation for the given key/current lang or undefined if not
+   */
+  getValue(key: string, callback: (value: GetValueFor) => void) {
+    return this.getValueFor(key, this.currentLang, callback)
+  }
+
+  /**
+   * Get a translation using a key for a given lang
+   * @param key the key of the wanted translation
+   * @param lang the lang which should have this key
+   * @returns the tranlsation for the given key/lang or undefined if not
+   */
+  getValueFor(key: string, lang: string, callback: (data: GetValueFor) => void) {
+    this.workerCall<GetValueFor>("getValueFor", [key, lang], (data) => {
+      callback(data)
+    })
+  }
+
+  /**
+   * Get a key from a value in the current lang.
+   * @param value the value which need to be finded to return is key
+   * @returns the key or udnefined if doesn't exist
+   */
+  getKey(value: string, callback: (key: GetKeyFor) => void) {
+    return this.getKeyFor(value, this.currentLang, callback)
+  }
+
+  /**
+   * Get a key from a value in the given lang.
+   * @param value the value which need to be finded to return is key
+   * @param lang the lang which should have this value
+   * @returns the key or udnefined if doesn't exist
+   */
+  getKeyFor(value: string, lang: string, callback: (data: GetKeyFor) => void) {
+    this.workerCall<GetKeyFor>("getFromValueForLang", [value, lang], (data) => {
+      callback(data)
+    })
   }
 
   /**
@@ -65,7 +106,7 @@ class LocalizedBrothers {
    * will happen.
    * @param lang The new lang you want to use (ex:"fr", "en", "ja", ...)
    */
-  setCurrentLang(lang: keyof typeof this.dictionary) {
+  setCurrentLang(lang: string) {
     const oldLang = this.currentLang;
     this.currentLang = lang
     this.translateAllIn(document.body, oldLang, lang);
@@ -80,25 +121,6 @@ class LocalizedBrothers {
   }
 
   /**
-   * Get a translation using a key for the current lang
-   * @param key the key of the wanted translation
-   * @returns the tranlsation for the given key/current lang or undefined if not
-   */
-  getFromKey(key: keyof typeof en) {
-    return this.getFromKeyForLang(key, this.currentLang)
-  }
-
-  /**
-   * Get a translation using a key for a given lang
-   * @param key the key of the wanted translation
-   * @param lang the lang which should have this key
-   * @returns the tranlsation for the given key/lang or undefined if not
-   */
-  getFromKeyForLang(key: keyof typeof en, lang: keyof typeof this.dictionary) {
-    return this.dictionary[lang][key]
-  }
-
-  /**
    * Translate an element from a specific lang to another. The innerHTML of this element will be searched in the translations
    * to get the translation key from the old language and translate it to another one.
    * @param element the element to translate
@@ -106,40 +128,28 @@ class LocalizedBrothers {
    * @param toLang the new lang which should replace the old one
    */
   translateElement(element: HTMLElement, fromLang: string, toLang: string) {
+    const startTime = new Date()
     const domText = element.innerHTML;
     //TODO: Optimisation work here to avoid translating things which don't need to
     //If no text, no translation
-    if(element.textContent == "" || element.textContent == undefined) return
+    if (element.textContent == "" || element.textContent == undefined) return
     //If same key, same text, stay inchanged
     var oldKey = element.dataset.i18nKey;
-    if(oldKey != undefined && oldKey == (i18n.dictionary as any)[this.currentLang][oldKey]){
-      return;
-    }
-    if(!isNaN(parseInt(element.textContent))) return
-
-    var uniqueIdClone = this.uniqueWorkerId++
-    var onTranslation = (event: MessageEvent<any>) => {
-      if(event.data.id != uniqueIdClone) return
-      (console as any).reverseLog(`[${fromLang} => ${toLang}] ${element.innerHTML} => ${event.data.translation}`)
-      if (event.data == undefined || event.data.translation == undefined) return
-      element.dataset.i18nKey = event.data.key; 
-      element.innerHTML = event.data.translation;
-    };
-
-    this.workerTranslator.addEventListener("message", onTranslation, {once:true});
-
-    //Proceed to the translation
-    this.workerTranslator.postMessage({
-      call:"translateString",
-      args:[domText, fromLang, toLang],
-      id: uniqueIdClone
+    this.getValueFor(oldKey, this.currentLang, (data) => {
+      if (oldKey != undefined && domText == data.value) {
+        return;
+      }
+      //If the text is calculation, numbers, don't need to translate
+      if (!isNaN(parseInt(element.textContent))) return
+      this.workerCall<TranslateStringCallback>("translateString", [domText, fromLang, toLang], (data) => {
+        const endTime = new Date();
+        (console as any).reverseLog(`[(${endTime.getTime() - startTime.getTime()}ms)  ${fromLang} => ${toLang}] ${element.innerHTML} => ${data.translation}`)
+        if (data == undefined || data.translation == undefined) return
+        element.dataset.i18nKey = data.key;
+        element.innerHTML = data.translation;
+      })
     })
 
-    //Proceed to the translation
-    // var data = this.translateString(domText, fromLang, toLang)
-    // if (data == undefined) return
-    // element.dataset.i18nKey = data.key; 
-    // element.innerHTML = data.translation;
   }
 
   /**
@@ -162,6 +172,21 @@ class LocalizedBrothers {
    */
   translateAll(fromLang: string, toLang: string) {
     this.translateAllIn(document.body, fromLang, toLang)
+  }
+
+  private workerCall<T>(fun: string, args: string[], callback: (data: T) => void) {
+    var uniqueIdClone = this.uniqueWorkerId++
+
+    this.workerTranslator.addEventListener("message", (ev) => {
+      if (uniqueIdClone != ev.data.id) return
+      callback(ev.data)
+    }, { once: true });
+
+    this.workerTranslator.postMessage({
+      call: fun,
+      args: args,
+      id: uniqueIdClone
+    })
   }
 }
 
--- src/ui/mods/mod_localized_brothers/translator.worker.ts
+++ src/ui/mods/mod_localized_brothers/translator.worker.ts
@@ -7,164 +7,175 @@ import fr from "./translations/fr.json"
 import en from "./translations/en.json"
 import { escapeRegExp } from "./utils/escapeRegExp"
 
+import { GetKeyFor, GetValueFor, TranslateStringCallback } from "./index"
+
 class LocalizedBrothersWorker {
-  dictionary = { fr, en }
+    dictionary = { fr, en }
 
-  constructor() {
+    constructor() {
 
-  }
-
-  /**
-   * Get a translation using a key for a given lang
-   * @param key the key of the wanted translation
-   * @param lang the lang which should have this key
-   * @returns the tranlsation for the given key/lang or undefined if not
-   */
-  getFromKeyForLang(key: keyof typeof en, lang: keyof typeof this.dictionary) {
-    return this.dictionary[lang][key]
-  }
-
-  /**
-   * Get a key from a value in the given lang.
-   * @param value the value which need to be finded to return is key
-   * @param lang the lang which should have this value
-   * @returns the key or udnefined if doesn't exist
-   */
-  getFromValueForLang(value: string, lang: keyof typeof this.dictionary) {
-    for (const key in this.dictionary[lang]) {
-      var _value = this.dictionary[lang][key as keyof typeof en]
-
-      //Build a regex which will accept everything as value between 2 "%".
-      var builtRegex = "^" + (escapeRegExp(_value)).replace(/\\\(%(.*?)%\\\)/g, ".*") + "$"
-      var finalRegex = new RegExp(builtRegex, "gm")
-
-      if (finalRegex.test(value)) return key
     }
-  }
 
-  /**
-   * Translate an element from a specific lang to another. The innerHTML of this element will be searched in the translations
-   * to get the translation key from the old language and translate it to another one.
-   * @param element the element to translate
-   * @param fromLang the old lang which should be the lang of the text in the given element
-   * @param toLang the new lang which should replace the old one
-   */
-  translateString(value: string, fromLang: string, toLang: string) {
-    const domText = value;
-    const domTextFormated = this.reverseTranslationParser(value);
-    var translationKey = this.getFromValueForLang(domTextFormated, fromLang as any);
-    if (!translationKey) {
-      return {translation:undefined, key:undefined}
-    }
-    var oldTranslation = this.getFromKeyForLang(translationKey as any, fromLang as any) as string
-    oldTranslation = this.translationParser(oldTranslation)
-    var data = this.dataExtractor(domText, oldTranslation)
-    data = this.translateData(data, fromLang, toLang);
-    var newText = this.getFromKeyForLang(translationKey as any, toLang as any);
-    newText = this.translationParser(newText)
-    newText = this.dataInjector(newText, data);
-    return {translation:newText, key:translationKey}
-  }
+    /**
+     * Get a key from a value in the given lang.
+     * @param value the value which need to be finded to return is key
+     * @param lang the lang which should have this value
+     * @returns the key or udnefined if doesn't exist
+     */
+    getKeyFor(value: string, lang: keyof typeof this.dictionary) {
+        for (const key in this.dictionary[lang]) {
+            var _value = this.dictionary[lang][key as keyof typeof en]
 
-  /**
-   * Translate the data if needed. For example if a data is a difficulty "beginner", we want to translate it.
-   * @param data a data array to translate
-   * @param fromLang from a given lang
-   * @param toLang to another lang
-   * @returns return the same array but translated if possible
-   */
-  translateData(data: string[], fromLang: string, toLang: string) {
-    data.forEach((v, i, o) => {
-      const dataKey = this.getFromValueForLang(v, fromLang as any)
-      if (dataKey) {
-        var newTranslation = this.getFromKeyForLang(dataKey as any, toLang as any)
-        if (newTranslation) {
-          o[i] = newTranslation
+            //Build a regex which will accept everything as value between 2 "%".
+            var builtRegex = "^" + (escapeRegExp(_value)).replace(/\\\(%(.*?)%\\\)/g, ".*") + "$"
+            var finalRegex = new RegExp(builtRegex, "gm")
+
+            if (finalRegex.test(value)) return key
         }
-      }
-    })
-    return data;
-  }
+        return;
+    }
 
-  /**
-   * Parse a string and return it formated
-   * @param value 
-   * @param data 
-   */
-  translationParser(value: string) {
-    //Break line
-    value = value.replace(/\n/g, "\n<br>")
+    /**
+     * Translate an element from a specific lang to another. The innerHTML of this element will be searched in the translations
+     * to get the translation key from the old language and translate it to another one.
+     * @param element the element to translate
+     * @param fromLang the old lang which should be the lang of the text in the given element
+     * @param toLang the new lang which should replace the old one
+     */
+    translateString(value: string, fromLang: string, toLang: string) {
+        const domText = value;
+        const domTextFormated = this.reverseTranslationParser(value);
+        var translationKey = this.getKeyFor(domTextFormated, fromLang as any);
+        if (!translationKey) return { translation: undefined, key: undefined }
+        var oldTranslation = this.getValueFor(translationKey as any, fromLang as any) as string
+        oldTranslation = this.translationParser(oldTranslation)
+        var data = this.dataExtractor(domText, oldTranslation)
+        data = this.translateData(data, fromLang, toLang);
+        var newText = this.getValueFor(translationKey as any, toLang as any);
+        if (!newText) return { translation: undefined, key: undefined };
+        newText = this.translationParser(newText)
+        newText = this.dataInjector(newText, data);
+        return { translation: newText, key: translationKey }
+    }
 
-    //Dialog
-    value = value.replace(/%SPEECH_ON%/g, "\n<br>\n<br><span style=\"color:#bcad8c\">\"")
-    value = value.replace(/%SPEECH_OFF%(.)/g, "\"</span>\n<br>\n<br>$1")
-    value = value.replace(/%SPEECH_OFF%/g, "\"</span>")
+    /**
+     * Get a translation using a key for a given lang
+     * @param key the key of the wanted translation
+     * @param lang the lang which should have this key
+     * @returns the tranlsation for the given key/lang or undefined if not
+     */
+    getValueFor(key: keyof typeof en, lang: keyof typeof this.dictionary) {
+        return this.dictionary[lang][key]
+    }
 
-    //Image
-    value = value.replace(/\[img\]/g, "<img src=\"coui://")
-    value = value.replace(/\[\/img\]/g, "\">")
+    /**
+     * Translate the data if needed. For example if a data is a difficulty "beginner", we want to translate it.
+     * @param data a data array to translate
+     * @param fromLang from a given lang
+     * @param toLang to another lang
+     * @returns return the same array but translated if possible
+     */
+    private translateData(data: string[], fromLang: string, toLang: string) {
+        data.forEach((v, i, o) => {
+            const dataKey = this.getKeyFor(v, fromLang as any)
+            if (dataKey) {
+                var newTranslation = this.getKeyFor(dataKey as any, toLang as any)
+                if (newTranslation) {
+                    o[i] = newTranslation
+                }
+            }
+        })
+        return data;
+    }
 
-    return value
-  }
+    /**
+     * Parse a string and return it formated
+     * @param value 
+     * @param data 
+     */
+    private translationParser(value: string) {
+        //Break line
+        value = value.replace(/\n/g, "\n<br>")
 
-  reverseTranslationParser(value: string): string {
-    //Dialog
-    value = value.replace(/\n<br>\n<br><span style=\"color:#bcad8c\">\"/g, "%SPEECH_ON%")
-    value = value.replace(/"<\/span>(\n<br>\n<br>)*/g, "%SPEECH_OFF%")
+        //Dialog
+        value = value.replace(/%SPEECH_ON%/g, "\n<br>\n<br><span style=\"color:#bcad8c\">\"")
+        value = value.replace(/%SPEECH_OFF%(.)/g, "\"</span>\n<br>\n<br>$1")
+        value = value.replace(/%SPEECH_OFF%/g, "\"</span>")
 
-    //Image
-    value = value.replace(/<img src=\"coui:\/\//g, "[img]")
-    value = value.replace(/\">/g, "[/img]")
+        //Image
+        value = value.replace(/\[img\]/g, "<img src=\"coui://")
+        value = value.replace(/\[\/img\]/g, "\">")
 
-    //Break line
-    value = value.replace(/\n<br>/g, "\n")
+        return value
+    }
 
-    return value
-  }
+    private reverseTranslationParser(value: string): string {
+        //Dialog
+        value = value.replace(/\n<br>\n<br><span style=\"color:#bcad8c\">\"/g, "%SPEECH_ON%")
+        value = value.replace(/"<\/span>(\n<br>\n<br>)*/g, "%SPEECH_OFF%")
 
-  /**
-   * Extract the data which is in the dom value based on the translation value and the postion of the placeholders
-   * @param domValue 
-   * @param translationValue 
-   * @returns 
-   */
-  dataExtractor(domValue: string, translationValue: string): string[] {
-    const splittedTranslatedValue = translationValue.split(/\(%.*?%\)/)
-    //sanitize regex
-    splittedTranslatedValue.forEach((r, i, o) => {
-      o[i] = escapeRegExp(r)
-    })
-    
+        //Image
+        value = value.replace(/<img src=\"coui:\/\//g, "[img]")
+        value = value.replace(/\">/g, "[/img]")
 
-    const regex = new RegExp(splittedTranslatedValue.join("(.*)"))
-    var result = regex.exec(domValue);
+        //Break line
+        value = value.replace(/\n<br>/g, "\n")
 
-    result?.shift()
-    return result ? result : [];
-  }
+        return value
+    }
 
-  dataInjector(translation: string, dataList: string[]) {
-    dataList.forEach((data) => {
-      translation = translation.replace(/\(%.*?%\)/, data)
-    })
-    return translation
-  }
+    /**
+     * Extract the data which is in the dom value based on the translation value and the postion of the placeholders
+     * @param domValue 
+     * @param translationValue 
+     * @returns 
+     */
+    private dataExtractor(domValue: string, translationValue: string): string[] {
+        const splittedTranslatedValue = translationValue.split(/\(%.*?%\)/)
+        //sanitize regex
+        splittedTranslatedValue.forEach((r, i, o) => {
+            o[i] = escapeRegExp(r)
+        })
+
+
+        const regex = new RegExp(splittedTranslatedValue.join("(.*)"))
+        var result = regex.exec(domValue);
+
+        result?.shift()
+        return result ? result : [];
+    }
+
+    private dataInjector(translation: string, dataList: string[]) {
+        dataList.forEach((data) => {
+            translation = translation.replace(/\(%.*?%\)/, data)
+        })
+        return translation
+    }
 }
 
 var instance = new LocalizedBrothersWorker()
 
 ctx.addEventListener("message", (event) => {
-    var returnData:any = null
+    var returnData: (GetKeyFor | GetValueFor | undefined | { id?: number }) & { id?: number } | undefined = undefined
     switch (event.data.call) {
         case "translateString":
-            returnData = instance.translateString(event.data.args[0], event.data.args[1], event.data.args[2])
+            returnData = instance.translateString(event.data.args[0], event.data.args[1], event.data.args[2]) as TranslateStringCallback
             break;
-        case "test":
-            returnData = event.data
+        case "getKeyFor":
+            returnData = {
+                key: instance.getKeyFor(event.data.args[0], event.data.args[1])
+            } as GetKeyFor
+            break;
+        case "GetValueFor":
+            returnData = {
+                value: instance.getValueFor(event.data.args[0], event.data.args[1])
+            } as GetValueFor
             break;
         default:
             break;
     }
+
+    if (!returnData) returnData = {}
     returnData.id = event.data.id
     ctx.postMessage(returnData);
 });
