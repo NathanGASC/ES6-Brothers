c4ff003e8882e35215b90ea8269b4852266d9c77
Nathan GASC <nathangasc17@gmail.com>
2023-03-26 01:55:43 +0100

    Squashed commit of the following:
    commit 1216d5e0c32c9aed58a496e21db8c338db0a37de
    Author: Nathan GASC <nathangasc17@gmail.com>
    Date:   Fri Mar 24 17:59:13 2023 +0100
    
        fix issue with zip
    
    commit 62e8931b8ab428028ee0385e64210afd6ebdc3e5
    Author: Nathan GASC <nathangasc17@gmail.com>
    Date:   Fri Mar 24 17:34:31 2023 +0100
    
        add Mod-Brothers to submodules
    
    commit 3021cebf206b7e6cf428f14c831a584e9874ddb4
    Author: Nathan GASC <nathangasc17@gmail.com>
    Date:   Sat Mar 18 16:59:28 2023 +0100
    
        edit readme
    
    commit 5005367e4a78a9a6bb9bd6ef35d290586b96c1c0
    Author: Nathan GASC <nathangasc17@gmail.com>
    Date:   Sat Mar 18 04:56:40 2023 +0100
    
        add polyfill core-js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const CopyPlugin = require("copy-webpack-plugin");
 const WatchExternalFilesPlugin = require('webpack-watch-files-plugin').default
-
+const exec = require('child_process').exec;
 const path = require("path");
 
 const isProduction = process.env.NODE_ENV == "production";
 const modName = process.env.MOD_NAME;
 const distFolder = process.env.DIST_FOLDER;
+const dataFolder = process.env.GAME_DATA_FOLDER
 
 const distModPath = `${distFolder}${modName}`
 
@@ -29,7 +30,18 @@ const config = {
       patterns: [
         { from: "./**/*", to: "./../../../[path][name][ext]", context: "./src", },
       ],
-    })
+    }),
+    {
+      apply: (compiler) => {
+        compiler.hooks.entryOption.tap('Zip', (compilation) => {
+          console.log(`npm run zip -- --src "${distModPath}" --output "${dataFolder}" ..."`)
+          exec(`npm run zip -- --src "${distModPath}" --output "${dataFolder}"`, (err, stdout, stderr) => {
+            if (stdout) process.stdout.write(stdout);
+            if (stderr) process.stderr.write(stderr);
+          });
+        });
+      }
+    }
   ],
   module: {
     rules: [
